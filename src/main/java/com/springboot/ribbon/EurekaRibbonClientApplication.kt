54f219ea11205ca91a4121c7962f7c6030e39634
talent.he <talent.liang.he@accenture.com>
2021-06-07 22:33:21 +0800

    add Hystrix dashboard

--- src/main/java/com/springboot/ribbon/EurekaRibbonClientApplication.kt
+++ src/main/java/com/springboot/ribbon/EurekaRibbonClientApplication.kt
@@ -1,14 +1,33 @@
 package com.springboot.ribbon
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletRegistrationBean
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+import org.springframework.context.annotation.Bean
 
 @EnableEurekaClient
 @SpringBootApplication
-//@EnableHystrix
-//@EnableHystrixDashboard
+@EnableHystrix
+@EnableHystrixDashboard
 class EurekaRibbonClientApplication {
+
+    // Hystrix dashboard
+    @Bean
+    fun getServlet(): ServletRegistrationBean<HystrixMetricsStreamServlet> {
+
+        var streamServlet = HystrixMetricsStreamServlet()
+        var registrationBean = ServletRegistrationBean(streamServlet)
+
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+
+        return registrationBean
+    }
 
     /*
     @Bean
